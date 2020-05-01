3d28f6d4cda21ac67abc9186fe1f04af53c605be
Samantha Richards <samantharichards@221b.local>
2020-04-30 23:09:23 -0400

    samantha's commit of extras.py and index.py with basic entity field

---
+++ cord_19_ems/es_module/extras.py
@@ -0,0 +1,77 @@
+import os
+import re
+import csv
+from collections import defaultdict
+import jsonlines
+import json
+
+
+# create a dict mapping sha's to entities and other metadata for all sources
+def all_ner_metadata_cross_reference(metadata_csv, ner_json, out):
+    # open json, get ents for each paper by doc_ids
+    ner_data = {}
+    with jsonlines.open(ner_json) as reader:
+        for obj in reader:
+            doc_id = obj['doc_id']
+            doc_sents = obj['sents']
+            doc_ents = defaultdict(list)
+            for sent in doc_sents:
+                ent_list = sent['entities']
+                for ent in ent_list:
+                    doc_ents[ent['type']].append(ent['text'])
+            ner_data[doc_id] = doc_ents
+
+    # open csv file containing the article ids (sha)
+    # the index of each line in the csv files corresponds to the doc_ids in the ner_json file
+    article_data = {}
+    with open(metadata_csv, 'r') as csvf:
+        reader = csv.reader(csvf)
+        next(reader)
+        for i, line in enumerate(reader):
+            sha = line[0]
+            source = line[1]
+            doi = line[3]
+            publish_time = line[8]
+            journal = line[10]
+            has_full_text = line[13]
+            if sha:
+                article_data[sha] = {"entities": ner_data[i]}
+                article_data[sha]['source'] = source
+                article_data[sha]['doi'] = doi
+                article_data[sha]['publish_time'] = publish_time
+                article_data[sha]['journal'] = journal
+                article_data[sha]['has_full_text'] = has_full_text
+
+    with open(out, 'w') as f:
+        json.dump(article_data, f)
+
+    return article_data
+
+
+def extract_year(publish_time):
+    m = re.match(r"[12][0-9][0-9][0-9]", publish_time)
+    if m:
+        yr = m.group(0)
+    else:
+        return 0
+    return yr
+
+
+def untokenize(ent_list):
+    if len(ent_list) > 0:
+        ents = " ".join([re.sub(r"{s}", "_", ent) for ent in ent_list])
+        return ents
+    else:
+        return ""
+
+
+def load_dataset_to_dict(data_dir):
+    articles = {}
+    for dirname, subdirs, files in os.walk(data_dir):
+        for file in files:
+            if file != '.DS_Store':
+                with open(os.path.join(dirname, file), 'r') as f:
+                    text_data = json.load(f)
+                    articles[text_data['paper_id']] = text_data
+
+    return articles
--- cord_19_ems/es_module/index.py
+++ cord_19_ems/es_module/index.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import json
 import time
 import glob
-
+import os
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 from elasticsearch_dsl import Index, Document, Text, Integer, Long, Nested, InnerDoc
@@ -11,52 +11,61 @@ from elasticsearch_dsl.analysis import analyzer, token_filter
 import networkx as nx
 from cord_19_ems.notebooks.Citation_Network import generate_citation_graph
 from collections import defaultdict
+import cord_19_ems.es_module.extras as utils                                        ### SMR ADDED
 
-# Connect to local host server
+# connect to local host server
 connections.create_connection(hosts=['127.0.0.1'])
 
-# Create elasticsearch object
+# create elasticsearch object
 es = Elasticsearch(timeout=30, max_retries=10, retry_on_timeout=True)
 
-# "text_analyzer" tokenizer splits at word boundaries, preserving internal hyphens.
+# data paths
+data_dir = '../data'                                                                ### SMR ADDED
+metadata_path = '../data_extras/all_sources_metadata_2020-03-13.csv'
+ner_path = '../data_extras/CORD-NER-ner.json'
+meta_ner_path = '../data_extras/cross_ref_data_all_sources.json'
 
+
+# "text_analyzer" tokenizer splits at word boundaries, preserving internal hyphens.
 # the additional custom filter breaks down hyphenated compound words into their subwords,
 # but also preserves the original hyphenated form.
-
 # the "flatten_graph" filter is necessary because the word delimiter graph filter can mess with
 # indexing by creating multi-position tokens. this can cause trouble for exact phrase matching.
-de_hyphenator = token_filter('de_hyphenator',type='word_delimiter_graph', preserve_original=True)
-text_analyzer = analyzer('custom',
-                       tokenizer='pattern', pattern=r'\b[\w-]+\b',
-                       filter=['lowercase','porter_stem', de_hyphenator, 'flatten_graph'])
+de_hyphenator = token_filter('de_hyphenator', type='word_delimiter_graph', preserve_original=True)
+text_analyzer = analyzer('custom', tokenizer='pattern', pattern=r"\b[\w-]+\b",
+                         filter=['lowercase', 'porter_stem', de_hyphenator, 'flatten_graph'])
+entity_analyzer = analyzer('custom', tokenizer='whitespace', filter=['lowercase'])  # ADDED BY SMR
 
 
-# Special datatype for author names. They contain a "first_name" and "last_name" field.
+# special datatype for author names. They contain a "first_name" and "last_name" field.
 class Name(InnerDoc):
     first = Text(analyzer='standard')
     last = Text()
 
-# Special datatype for Citations They contain a "title" and "year" field.
+
+# special datatype for Citations They contain a "title" and "year" field.
 class Citation(InnerDoc):
     title = Text()
     year = Integer()
 
+
 class Article(Document):
     id_num = Text(analyzer='standard')
-    # authors field is a Nested list of Name objects
-    authors = Nested(Name)
+    authors = Nested(Name)                  # authors field is a Nested list of Name objects
     title = Text(analyzer=text_analyzer, boost=3)
     abstract = Text(analyzer=text_analyzer)
     body = Text(analyzer=text_analyzer)
-    # citations field is a Nested list of Citation objects
-    citations = Nested(Citation)
+    citations = Nested(Citation)            # citations field is a Nested list of Citation objects
     pr = Long()
+    gene_or_genome = Text(analyzer=entity_analyzer)
+    publish_time = Integer()
 
     # override the Document save method to include subclass field definitions
     def save(self, *args, **kwargs):
         return super(Article, self).save(*args, **kwargs)
 
-# Populate the index
+
+# populate the index
 def buildIndex():
     """
     buildIndex creates a new film index, deleting any existing index of
@@ -69,79 +78,72 @@ def buildIndex():
     pagerank_scores = nx.pagerank(citation_graph)
     ddict = defaultdict(float, pagerank_scores)
 
-    article_index = Index('covid_index')
+    article_index = Index('another_covid_index')
     if article_index.exists():
-        article_index.delete()  # Overwrite any previous version
+        article_index.delete()  # overwrite any previous version
     article_index.document(Article)  # register the document mapping
     article_index.create()
 
-    # Open the json film corpus
-    articles = dict()
-    i = 0
-    for file in glob.iglob('../data/comm_use_subset/pdf_json' + "/*.json"):
-        with open(file, 'r', encoding='utf-8') as f:
-            # a single article document
-            doc = json.loads(f.read())
-            articles[str(i)] = doc
-        i += 1
-    for file in glob.iglob('../data/comm_use_subset/pmc_json' + "/*.json"):
-        with open(file, 'r', encoding='utf-8') as f:
-            # a single article document
-            doc = json.loads(f.read())
-            articles[str(i)] = doc
-        i += 1
-    for file in glob.iglob("../data/noncomm_use_subset" + "/*.json"):
-        with open(file, 'r', encoding='utf-8') as f:
-            # a single article document
-            doc = json.loads(f.read())
-            articles[str(i)] = doc
-        i += 1
+    # load articles
+    articles = utils.load_dataset_to_dict(data_dir)
+    print("SIZE OF ARTICLES: ", len(articles.keys()))
 
-    for file in glob.iglob("../data/biorxiv_medrxiv/pdf_json" + "/*.json"):
-        with open(file, 'r', encoding='utf-8') as f:
-            # a single article document
-            doc = json.loads(f.read())
-            articles[str(i)] = doc
-        i += 1
-
-    size = len(articles)
+    # open ner and metadata dict
+    with open(meta_ner_path, 'r') as f:
+        meta_ner_all = json.load(f)
+    print("SIZE OF META DATA: ", len(meta_ner_all.keys()))
 
     def actions():
-        for mid in range(1, size):
-            article = articles[str(mid)]
-            try:
-                citations = article['bib_entries']
-            except KeyError:
-                citations= [{}]
-            try:
-                title = article['metadata']['title']
-            except KeyError:
-                title = ''
-            try:
-                abstract = ' '.join([abs['text'] for abs in article['abstract']])
-            except KeyError:
-                abstract = ''
+        for i, article in enumerate(articles.values()):
+            sha = article['paper_id']
+
+            # extract contents of entity and metadata dict
+            if sha in meta_ner_all.keys():  # entities, source, doi, publish_time, has_full_text, journal
+                ent_types = meta_ner_all[sha]['entities']
+                publish_time = utils.extract_year(meta_ner_all[sha]["publish_time"])
+                gene_or_genome = utils.untokenize(ent_types['GENE_OR_GENOME']) \
+                    if 'GENE_OR_GENOME' in ent_types.keys() else ''
+            else:
+                publish_time = 0
+                gene_or_genome = ''
+
+            # extract contents of article dict
+            title = article['metadata']['title'] if 'title' in article['metadata'].keys() else ''
+            cits = article['bib_entries'] if 'bib_entries' in article.keys() else [{}]
+            cits = [{"title": cit['title'], "year": cit['year']} for cit in cits.values() if cit['title'] != '']
+            authors = [{"first": auth['first'], "last": auth["last"]} for auth in article['metadata']['authors']]
+            pr = ddict[article['metadata']['title']]
+            abstract = ' '.join([abs['text'] if 'text' in abs.keys() else '' for abs in article['abstract']])
+            body = ' '.join([sect['text'] for sect in article['body_text']])
+
+            # TEST FIELDS HERE
+            #print(f"\ntitle {title}\ncitations {cits}\nauthors {authors}\nyear {publish_time}\npr {pr}"
+            #      f"\nabstract {abstract}\ngenes {gene_or_genome}\n")
+
             yield {
-                "_index": "covid_index",
+                "_index": "another_covid_index",
                 "_type": '_doc',
-                "_id": mid,
+                "_id": i,
                 "title": title,
-                "id_num": article['paper_id'],
+                "id_num": sha,
                 "abstract": abstract,
-                "authors": [{"first": author['first'], "last": author["last"]}
-                            for author in article['metadata']['authors']],
-                "body": ' '.join([sec['text'] for sec in article['body_text']]),
-                # ignore citations with no title; they cause problems comparing documents.
-                "citations": [{"title": citations[bib]['title'], "year": citations[bib]['year']}
-                              for bib in citations.keys() if citations[bib]['title'] !='' ],
-                "pr": ddict[article['metadata']['title']],
+                "body": body,
+                "authors": authors,
+                "publish_time": publish_time,
+                "citations": cits,
+                "pr": pr,
+                "gene_or_genome": gene_or_genome
             }
+
     helpers.bulk(es, actions(), raise_on_error=True) # one doc in corpus contains a NAN value and it has to be ignored.
 
 
 # command line invocation builds index and prints the running time.
 def main():
     start_time = time.time()
+    # if extra datafiles have not been cross-referenced, do this
+    if not os.path.isfile(meta_ner_path):
+        utils.all_ner_metadata_cross_reference(metadata_path, ner_path, meta_ner_path)
     buildIndex()
     print("=== Built index in %s seconds ===" % (time.time() - start_time))
 
